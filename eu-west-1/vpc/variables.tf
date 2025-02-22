cd62e1e442f29f9c38ea6a112b896ae187194129
Kurdeep <132860667+Kurdeep@users.noreply.github.com>
2025-02-22 11:45:46 +0530

    Create variables.tf

---
+++ eu-west-1/vpc/variables.tf
@@ -0,0 +1,26 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+}
+
+variable "enable_nat_gateway" {
+  description = "Enable NAT Gateway for private subnets"
+  type        = bool
+  default     = true
+}
